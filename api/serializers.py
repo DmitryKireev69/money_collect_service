8ace4a7a020ec3e79fb415badc3b6856faa6a12e
kireev.d <kireev.d69@yandex.ru>
2025-12-04 02:22:02 +0300

    Добавляем в серилизатор платежа ФИО пользователя

--- api/serializers.py
+++ api/serializers.py
@@ -12,7 +12,23 @@ class CollectSimpleSerializer(serializers.ModelSerializer):
 
 class PaymentSimpleSerializer(serializers.ModelSerializer):
     """Сериализатор для Payment"""
+    user_full_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Payment
         fields = '__all__'
+
+    def get_user_full_name(self, obj):
+        """
+        Возвращает ФИО пользователя или 'Аноним'
+        """
+        if obj.is_anonymous:
+            return "Аноним"
+
+        if not obj.user:
+            return "Гость"
+
+        first_name = obj.user.first_name or ''
+        last_name = obj.user.last_name or ''
+        full_name = f"{first_name} {last_name}".strip()
+        return full_name if full_name else obj.user.username
