64172f98d9c2c5face65ddeeb04b78804215fc58
kireev.d <kireev.d69@yandex.ru>
2025-12-04 01:14:44 +0300

    Добавление роутеров на вью

--- collect_service/urls.py
+++ collect_service/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from api.views import CollectViewSet, PaymentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'collects', CollectViewSet)
+router.register(r'payments', PaymentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path('', include(router.urls)),
 ]
 
 if settings.DEBUG:
