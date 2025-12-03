7463187416c9b5b7585c74ece75ea2a0df4d7b4e
kireev.d <kireev.d69@yandex.ru>
2025-12-04 01:14:56 +0300

    Добавление стандартных вью

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Collect, Payment
+from .serializers import (
+    CollectSimpleSerializer,
+    PaymentSimpleSerializer
+)
 
-# Create your views here.
+
+class CollectViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с групповыми сборами
+    """
+    queryset = Collect.objects.all()
+    serializer_class = CollectSimpleSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet для работы с групповыми сборами
+    """
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSimpleSerializer
