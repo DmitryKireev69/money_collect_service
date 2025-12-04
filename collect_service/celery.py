8866ab4fd3a5a995869230f940c4abf2cb740e64
kireev.d <kireev.d69@yandex.ru>
2025-12-04 17:51:49 +0300

    Подключение селеот

---
+++ collect_service/celery.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collect_service.settings')
+app = Celery('money_collect')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
