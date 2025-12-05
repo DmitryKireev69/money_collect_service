b1ebe47d3fd7d8b1e3efcfc97f30fc229c21ff75
kireev.d <kireev.d69@yandex.ru>
2025-12-05 04:06:52 +0300

    Добавление создания супер пользователя

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,21 @@ done
 echo "DB is ready!"
 
 python manage.py migrate --noinput
+
+echo "Создание супер пользователя...."
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
 python manage.py runserver 0.0.0.0:8000
