b5e3afd3f9648bdfb43df9fe32db0fc8ae69977e
kireev.d <kireev.d69@yandex.ru>
2025-12-08 15:25:44 +0300

    Добавление логов и приявязка таска к приложению

--- api/tasks.py
+++ api/tasks.py
@@ -1,15 +1,26 @@
 # api/tasks.py
-from celery import shared_task
+import logging
 from django.core.mail import send_mail
+from collect_service.celery import app
 
+log = logging.getLogger(__name__)
 
-@shared_task
-def send_email(subject, message, recipient):
-    """Простая задача отправки email"""
+@app.task
+def send_email_task(subject, message, recipient):
+    """Задача отправки email"""
+    log.info(
+        f"Уведомление по электронной почте пользователю c email:{recipient}"
+    )
     send_mail(
         subject=subject,
         message=message,
         from_email='noreply@moneycollect.com',
         recipient_list=[recipient],
         fail_silently=True,
+    )
+
+    log.info(f"Отправка на email {recipient}")
+
+    log.info(
+        f"Отправка на email {recipient} завершена!",
     )
